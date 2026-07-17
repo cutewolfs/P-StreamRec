d8e3067187ad21c9235239f52db302b1990082b0
Sylvain Dupuy <sylvain.dupuy98@gmail.com>
2026-07-17 19:06:25 +0200

    restore br

--- app/tasks/monitor.py
+++ app/tasks/monitor.py
@@ -118,7 +118,7 @@ async def check_model_status(
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
             "Accept": "application/json",
             "Accept-Language": "en-US,en;q=0.9",
-            "Accept-Encoding": "gzip, deflate",
+            "Accept-Encoding": "gzip, deflate, br",
             "Referer": "https://chaturbate.com/",
             "Origin": "https://chaturbate.com",
             "Connection": "keep-alive",
